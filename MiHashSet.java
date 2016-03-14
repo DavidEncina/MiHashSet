29b995e901231d661a18dc6e2a21634d099ef1f2
David <rulateunmai@hotmail.com>
2016-03-14 12:23:19 +0100

    Añadido constructor e importadas clases ArrayListInt y Test

---
+++ MiHashSet.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class MiHashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashSet
+{
+    // Es una lista que almacena enteros
+    private ArrayListInt lista;
+
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet()
+    {
+        lista = new ArrayListInt();
+    }
+}
